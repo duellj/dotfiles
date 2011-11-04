ab7f016ba67585fb911af4469ff561f8305369a6
Jon Duell <jon@jonduell.com>
2011-11-04 14:31:09 -0700

    Updates

--- .drushrc.php
+++ .drushrc.php
@@ -59,7 +59,7 @@
 # $options['c'] = '.';
 
 // Disable the nag warning for Windows.
-// Consider improving Windows support: http://drupal.org/node/766080. 
+// Consider improving Windows support: http://drupal.org/node/766080.
 # $options['check_os'] = FALSE;
 
 // Control automatically check for updates in pm-updatecode and drush version.
@@ -119,7 +119,7 @@
 // version of rsync installed on any system you are using with
 // drush).  Note that drush requires at least rsync version 2.6.4
 // for some functions to work correctly.
-// 
+//
 // Note that this option can also be set in a site alias.  This
 // is preferable if newer versions of rsync are available on some
 // of the systems you use.
@@ -140,7 +140,7 @@
  * Multiple command execution options
  */
 // By default, drush will prepend the name of the
-// site to the output of any multiple-site command 
+// site to the output of any multiple-site command
 // execution.  To disable this behavior, set the
 // --no-label option
 # $options['no-label'] = TRUE;
@@ -195,10 +195,13 @@ $options['skip-tables'] = array(
 # $command_specific['pm-updatecode'] = array('notes' => TRUE);
 
 // Save backups to hidden directory.
-$command_specific['pm-update'] = array('backup-dir' => '/Users/jonduell/.drush-backups');
+$command_specific['pm-update'] = array(
+  # 'backup-dir' => '/Users/jonduell/.drush-backups',
+  'no-backup' => TRUE,
+);
 
-// List of drush commands or aliases that should override built-in 
-// shell functions and commands; otherwise, built-ins override drush 
+// List of drush commands or aliases that should override built-in
+// shell functions and commands; otherwise, built-ins override drush
 // commands. Default is help,dd,sa.
 // Warning:  bad things can happen if you put the wrong thing here
 // (e.g. eval, grep), so be cautious.
--- .zshrc
+++ .zshrc
@@ -72,6 +72,15 @@ reloadfun() {
   autoload -U $f:t
 }
 
+# Rename modules (filename and function names).
+module_rename() {
+  if (( $# == 0 ))
+  then echo usage: module_rename old_name new_name; fi
+  autoload zmv;
+  zmv '*' "\$f:s/${1}/${2}";
+  perl -pi -e "s/${1}/${2}/" *;
+}
+
 # PostgreSQL
 export PGDATA=/Library/PostgreSQL/8.4/data
 
